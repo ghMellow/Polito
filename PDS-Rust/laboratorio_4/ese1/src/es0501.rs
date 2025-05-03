e16bee335e1cf4772099704b3c2e392f222fa4f5
ghMellow <nicotermins@gmail.com>
2025-05-03 19:13:36 +0200

    Finished ese1

--- PDS-Rust/laboratorio_4/ese1/src/es0501.rs
+++ PDS-Rust/laboratorio_4/ese1/src/es0501.rs
@@ -48,6 +48,7 @@ pub mod mem_inspect {
 
 
 pub mod List1 {
+    use std::mem;
 
     pub enum Node<T> {
         Cons(T, Box<Node<T>>),
@@ -55,12 +56,19 @@ pub mod List1 {
     }
 
     pub struct List<T> {
+        /// Usiamo mem::replace perchè non si può spostare un elemento della struttura. O si sposta tutto o niente
+        /// mentre modificare un elemento solo si può fare
         head: Node<T>,
     }
 
-    impl<T> List<T> {
+    // Definisci la struttura ListIter che conterrà un riferimento al nodo corrente
+    pub struct ListIter<'a, T> {
+        current: &'a Node<T>,
+    }
+
+    impl<T: std::fmt::Display> List<T> {
         pub fn new() -> Self {
-            unimplemented!()
+            List{ head: Node::Nil } // Nil è equivalente di Null
         }
 
         // insert a new element at the beginning of the list
@@ -71,48 +79,89 @@ pub mod List1 {
         // 3. you can't copy it either, because Box can't be copied
         // solution: use mem::replace to move the value of self.head into a new variable and replace it with Nil
         // 4. let self.head point to the new created node
-        pub fn push(&mut self, elem: i32) {
-            unimplemented!()
+        pub fn push(&mut self, elem: T) {
+            let old_head = mem::replace(&mut self.head, Node::Nil);
+            self.head = Node::Cons(elem, Box::new(old_head));
         }
 
         // pop the first element of the list and return it
-        fn pop(&mut self) -> Option<T> {
-            unimplemented!()
+        pub fn pop(&mut self) -> Option<T> {
+            let enum_pair = mem::replace(&mut self.head, Node::Nil);
+
+            /// Ricorda! Gli 'enum' vanno acceduti con un pattern matching
+            match enum_pair {
+                Node::Cons(head, tail) => {
+                    self.head = *tail; // asterisco per deferenziare il box nel heap
+                                       // è un nodo enum ovvero facendo questa assegnazione in automatico avrò testa e coda
+                    Some(head)
+                }
+                Node::Nil => {None}
+            }
         }
 
-        // return a referece to the first element of the list
+        // return a reference to the first element of the list
         pub fn peek(&self) -> Option<&T> {
-            unimplemented!()
+            /// Ricorda! Gli 'enum' vanno acceduti con un pattern matching
+            match &self.head {
+                Node::Cons(head, tail) => {
+                    Some(head)
+                },
+                Node::Nil => None,
+            }
         }
 
         // uncomment after having implemented the ListIter struct
-        // return an interator over the list values
-        //fn iter(&self) -> ListIter<T> {
-        //    unimplemented!()
-        //}
+        // return an iterator over the list values
+        pub fn iter(&self) -> ListIter<T> {
+            /// Metodo per ottenere un iteratore immutabile della lista
+            ListIter{ current: &self.head }
+        }
 
         // take the first n elements of the list and return a new list with them
-        pub fn take(&mut self, n: usize) -> List<T> {
-            unimplemented!()
+        pub fn take(&mut self, n: usize) -> List<T> where T: Clone{
+            let mut temp_res = Vec::new();
+            let mut res = List::new();
+            let mut count = 0;
+
+            let mut iter = self.iter(); // create iterator once
+
+            while let Some(next) = iter.next() {
+                if count == n {
+                    break;
+                }
+                temp_res.push(next.clone());
+                count += 1;
+            }
+
+            // reverse the vector before pushing to maintain order
+            temp_res.reverse();
+
+            for value in temp_res {
+                res.push(value);
+            }
+
+            res
         }
     }
 
-    //struct ListIter {
-    //    // implement the iterator trait for ListIter
-    //}
-    //
-    //impl Iterator for ListIter {
-    //    //type Item = ...
-    //
-    //    fn next(&mut self) -> Option<Self::Item> {
-    //        unimplemented!()
-    //    }
-    //}
+    // Implementazione del trait Iterator per ListIter
+    impl<'a, T> Iterator for ListIter<'a, T> {
+        // Il tipo di elemento che l'iteratore produce
+        type Item = &'a T;
 
-    // something that may be useful for the iterator implementation:
-    // let a = Some(T);
-    // let b = &a;
-    // match b { Some(i) => ... } // here i is a reference to T
+        // Metodo principale dell'iteratore che restituisce il prossimo elemento
+        fn next(&mut self) -> Option<Self::Item> {
+            match self.current {
+                Node::Cons(value, next) => {
+                    // Sposta il puntatore al prossimo nodo
+                    self.current = next;
+                    // Restituisci un riferimento al valore corrente
+                    Some(value)
+                },
+                Node::Nil => None, // Fine della lista
+            }
+        }
+    }
 }
 
 pub mod List2 {
@@ -122,38 +171,119 @@ pub mod List2 {
         next: NodeLink<T>,
     }
 
-    type NodeLink<T> = Option<Box<Node<T>>>;
+    type NodeLink<T> = Option<Box<Node<T>>>; // alias
 
     pub struct List<T> {
         head: NodeLink<T>,
     }
 
-    // for this implementattion, since we are using option, take a look at the take method in Option<T>.
+    /// Reference & al valore corrente della lista
+    pub struct ListIter<'a, T> {
+        current: &'a NodeLink<T>,
+    }
+
+    // for this implementation, since we are using option, take a look at the take method in Option<T>.
     // It allows to move the value of the option into another option and replace it with None
     // let mut a = Some(5);
     // let b = a.take(); // a is now None and b is Some(5)
     impl<T> List<T> {
         // same methods as List1
+        pub fn new() -> Self {
+            List{ head: None }
+        }
+        pub fn push(&mut self, elem: T) {
+            let mut old_head = self.head.take();
+            self.head = NodeLink::Some(Box::new(Node{elem: elem, next: old_head}))
+        }
+        pub fn pop(&mut self) -> Option<T> {
+
+            // if let Some(value) = self.head.take() {
+            //     self.head = value.next;
+            //     Some(value.elem)
+            // } else {
+            //     None
+            // }
+
+            match self.head.take() { /// Prendo val e metto none con take
+                None => {None}
+                Some(value) => {
+                    self.head = value.next; /// Nuovo val
+                    Some(value.elem)
+                }
+            }
+        }
+        pub fn peek(&self) -> Option<&T> {
+            match &self.head{
+                None => {None}
+                Some(value) => {
+                    Some(&value.elem)
+                }
+            }
+        }
+
+        pub fn iter(&self) -> ListIter<T> {
+            /// Metodo per ottenere un iteratore immutabile della lista
+            ListIter { current: &self.head }
+        }
+        pub fn take(&mut self, n: usize) -> List<T> where T: Clone{
+            let mut res = List::new();
+            let mut temp_res = Vec::new();
+
+            for (count, next) in self.iter().enumerate() {
+                if count == n {
+                    break;
+                }
+
+                temp_res.push(next.clone());
+            }
+
+            temp_res.reverse();
+            for value in temp_res {
+                res.push(value);
+            }
+
+            res
+        }
+    }
+
+    impl<'a, T> Iterator for ListIter<'a, T> {
+        type Item = &'a T;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            match self.current {
+                Some(nodeLink) => {
+                    self.current = &nodeLink.next;
+                    Some(&nodeLink.elem)
+                },
+                None => {None}
+            }
+        }
     }
 }
 
 pub mod dlist {
+    // problema: nello stack non puoi avere più puntatori che puntano allo stesso elemento nel heap
+    // usare i puntatori dinamici
 // *****
 // double linked list suggestions:
 // the node has both a next and a prev link
 
-// type NodeLink = ???
-// typer NodeBackLink = ???
-// struct DNode<T> {
-//     elem: T,
-//     prev: NodeBackLink,  // which type do we use here?
-//     next: NodeLink, // which type do we use here?
-// }
+    use std::cell::RefCell;
+    use std::rc::{Rc, Weak};
+    use crate::es0501::List1::Node::Nil;
 
-// struct DList {
-// head: NodeLink,
-// tail: NodeLink
-// }
+    struct DNode<T> {
+        elem: T,
+        prev: NodeBackLink<T>,  // which type do we use here?
+        next: NodeLink<T>, // which type do we use here?
+    }
+    type NodeLink<T> = Option<Rc<RefCell<DNode<T>>>>;
+    type NodeBackLink<T> = Option<Weak<RefCell<DNode<T>>>>;
+
+    struct DList<T> {
+        head: NodeLink<T>,
+        tail: NodeBackLink<T>
+    }
 
 // use Rc, since we need more than one reference to the same node. 
 // You need to both strong and weak references
@@ -178,5 +308,134 @@ pub mod dlist {
 // Some(T).clone() ->  Some(T.clone())
 // None.clone() -> None
 
+    impl<T> DList<T> {
+        pub fn new() -> Self {
+            DList{ head: None, tail: None }
+        }
+        pub fn push_front(&mut self, elem: T) {
+            let new_head = Rc::new(RefCell::new(DNode {
+                elem,
+                prev: None,
+                next: self.head.clone(),
+            }));
 
+            // Aggiorna il prev del vecchio head, se esiste
+            if let Some(ref old_head) = self.head {
+                old_head.borrow_mut().prev = Some(Rc::downgrade(&new_head));
+            } else {
+                // Lista vuota: aggiorna anche il tail (deve essere un Weak pointer)
+                self.tail = Some(Rc::downgrade(&new_head));
+            }
+
+            // Aggiorna il head con il nuovo nodo
+            self.head = Some(new_head);
+        }
+
+        pub fn pop_front(&mut self) -> Option<T> {
+            // Prendiamo possesso del nodo head
+            self.head.take().and_then(|old_head| {
+                // Estrai il valore interno
+                let mut old_head_ref = old_head.borrow_mut();
+
+                // Per prendere possesso dell'elemento, usiamo std::mem::replace
+                // che sostituisce elem con un valore dummy temporaneo
+                let old_elem = std::mem::replace(&mut old_head_ref.elem, unsafe { std::mem::zeroed() });
+
+                // Aggiorniamo head al prossimo nodo
+                match old_head_ref.next.take() {
+                    Some(new_head) => {
+                        // Il nuovo head non deve avere prev
+                        new_head.borrow_mut().prev = None;
+                        self.head = Some(new_head);
+                    }
+                    None => {
+                        // La lista è ora vuota
+                        self.tail = None;
+                    }
+                }
+
+                // Rilascia il borrow_mut prima della fine dello scope
+                drop(old_head_ref);
+
+                // A questo punto, old_head viene droppato, poiché non lo usiamo più
+                Some(old_elem)
+            })
+        }
+
+        pub fn push_back(&mut self, elem: T) {
+            // Crea il nuovo nodo
+            let new_node = Rc::new(RefCell::new(
+                DNode {
+                    elem: elem,
+                    prev: self.tail.clone(), // Mantiene il riferimento al vecchio tail
+                    next: None,
+                }
+            ));
+
+            // Se c'è un vecchio tail, collega il suo next al nuovo nodo
+            if let Some(ref old_tail) = self.tail {
+                if let Some(old_tail_strong) = old_tail.upgrade() {
+                    old_tail_strong.borrow_mut().next = Some(new_node.clone());
+                }
+            } else {
+                // Se non c'è un vecchio tail, la lista è vuota
+                // quindi il nuovo nodo diventa anche head
+                self.head = Some(new_node.clone());
+            }
+
+            // Il nuovo nodo diventa il nuovo tail
+            self.tail = Some(Rc::downgrade(&new_node));
+        }
+    }
+
+    /**
+
+    TEST
+
+    **/
+
+    #[test]
+    fn test_push_front() {
+        let mut list = DList::new();
+        list.push_front(10);
+        list.push_front(20);
+        list.push_front(30);
+
+        assert_eq!(list.pop_front(), Some(30));
+        assert_eq!(list.pop_front(), Some(20));
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_push_back() {
+        let mut list = DList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_mixed_push() {
+        let mut list = DList::new();
+        list.push_front(1); // list: [1]
+        list.push_back(2);  // list: [1, 2]
+        list.push_front(0); // list: [0, 1, 2]
+
+        assert_eq!(list.pop_front(), Some(0));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_empty_list() {
+        let mut list: DList<i32> = DList::new();
+        assert_eq!(list.pop_front(), None);
+    }
 }
--- PDS-Rust/laboratorio_4/ese1/src/lib.rs
+++ PDS-Rust/laboratorio_4/ese1/src/lib.rs
@@ -1,1 +1,112 @@
 pub mod es0501;
+mod es0502;
+
+#[cfg(test)]
+pub mod test_list_1{
+    use super::es0501::List1::{List};
+
+    #[test]
+    fn test_push_and_peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+
+        list.push(10);
+        assert_eq!(list.peek(), Some(&10));
+
+        list.push(20);
+        assert_eq!(list.peek(), Some(&20));
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_take() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut new_list = list.take(2);
+        println!("{:?}", new_list.pop());
+        println!("{:?}", new_list.pop());
+        println!("{:?}", new_list.pop());
+    }
+}
+#[cfg(test)]
+pub mod test_list_2{
+
+    use super::es0501::List2::{List};
+
+    #[test]
+    fn test_push_and_peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+
+        list.push(1);
+        assert_eq!(list.peek(), Some(&1));
+
+        list.push(2);
+        assert_eq!(list.peek(), Some(&2));
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut list = List::new();
+        list.push(10);
+        list.push(20);
+
+        assert_eq!(list.pop(), Some(20));
+        assert_eq!(list.pop(), Some(10));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3); // List is now [3, 2, 1]
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_take() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut new_list = list.take(2);
+        println!("{:?}", new_list.pop());
+        println!("{:?}", new_list.pop());
+        println!("{:?}", new_list.pop());
+    }
+}
